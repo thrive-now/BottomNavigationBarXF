154491ce7db1ad9a193c4f89003de52bd6f6410d
mikescandy <michele.scandura@outlook.com>
2017-09-21 13:48:56 +0100

    updates

--- example/BottomBarXFExample/App.xaml.cs
+++ example/BottomBarXFExample/App.xaml.cs
@@ -13,35 +13,39 @@ namespace BottomBarXFExample
 			InitializeComponent ();
 
 			BottomBarPage bottomBarPage = new BottomBarPage ();
-			bottomBarPage.BarBackgroundColor = Color.Pink;
+			//bottomBarPage.BarBackgroundColor = Color.Pink;
 
 
-			// You can only define the color for the active icon if you set the Bottombar to fixed mode
-			// So if you want to try this, just uncomment the next two lines
+            // You can only define the color for the active icon if you set the Bottombar to fixed mode
+            // So if you want to try this, just uncomment the next two lines
 
-			//bottomBarPage.BarTextColor = Color.Blue; // Setting Color of selected Text and Icon
-			//bottomBarPage.FixedMode = true;
+            //bottomBarPage.BarTextColor = Color.Blue; // Setting Color of selected Text and Icon
+            //bottomBarPage.FixedMode = true;
 
-			// Whith BarTheme you can select between light and dark theming when using FixedMode
-			// When using DarkTheme you can set the Background Color by adding a colors.xml to you Android.Resources.Values
-			// with content
-			//
-			//  <color name="white">#ffffff</color>
-			//  < color name = "bb_darkBackgroundColor" >#000000</color>
-			//
-			// by setting "white" you can select the color of the non selected items and texts in dark theme
-			// The Difference between DarkThemeWithAlpha and DarkThemeWithoutAlpha is that WithAlpha will draw not selected items with halfe the 
-			// intensity instaed of solid "white" value
-			//
-			// Uncomment next line to use Dark Theme
-			// bottomBarPage.BarTheme = BottomBarPage.BarThemeTypes.DarkWithAlpha; 
+            // Whith BarTheme you can select between light and dark theming when using FixedMode
+            // When using DarkTheme you can set the Background Color by adding a colors.xml to you Android.Resources.Values
+            // with content
+            //
+            //  <color name="white">#ffffff</color>
+            //  < color name = "bb_darkBackgroundColor" >#000000</color>
+            //
+            // by setting "white" you can select the color of the non selected items and texts in dark theme
+            // The Difference between DarkThemeWithAlpha and DarkThemeWithoutAlpha is that WithAlpha will draw not selected items with halfe the 
+            // intensity instaed of solid "white" value
+            //
+            // Uncomment next line to use Dark Theme
+            // bottomBarPage.BarTheme = BottomBarPage.BarThemeTypes.DarkWithAlpha;
 
-			string[] tabTitles = { "Favorites", "Friends", "Nearby", "Recents", "Restaurants" };
+            string[] tabTitles = { "Favorites", "Friends", "Nearby", "Recents", "Restaurants" };
 			string [] tabColors = { null, "#5D4037", "#7B1FA2", "#FF5252", "#FF9800" };
+		    int[] tabBadgeCounts = {0, 1, 5, 3, 4};
+		    string[] tabBadgeColors = {"#000000", "#FF0000", "#000000", "#000000", "#000000"};
 
 			for (int i = 0; i < tabTitles.Length; ++i) {
 				string title = tabTitles [i];
 				string tabColor = tabColors [i];
+			    int tabBadgeCount = tabBadgeCounts[i];
+			    string tabBadgeColor = tabBadgeColors[i];
 
 				FileImageSource icon = (FileImageSource) FileImageSource.FromFile (string.Format ("ic_{0}.png", title.ToLowerInvariant ()));
 
@@ -55,6 +59,10 @@ namespace BottomBarXFExample
 				if (tabColor != null) {
                     BottomBarPageExtensions.SetTabColor(tabPage, Color.FromHex(tabColor));
 				}
+
+                // Set badges
+                BottomBarPageExtensions.SetBadgeCount(tabPage, tabBadgeCount);
+                BottomBarPageExtensions.SetBadgeColor(tabPage, Color.FromHex(tabBadgeColor));
 
 				// set label based on title
 				tabPage.UpdateLabel ();
